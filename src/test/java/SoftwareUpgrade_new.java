608dd1b85b8865aa18279a1d523c13af40c56361
kristinaryzh <39327896+kristinaryzh@users.noreply.github.com>
2018-05-17 12:20:16 +0300

    1

--- src/test/java/SoftwareUpgrade_new.java
+++ src/test/java/SoftwareUpgrade_new.java
@@ -1,24 +1,25 @@
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 
-public class SoftwareUpgrade {
+
+public class SoftwareUpgrade_new {
     FirefoxDriver wd;
     
     @BeforeMethod
     public void setUp() throws Exception {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+        wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
     
     @Test
-    public void SoftwareUpgrade() {
+    public void SoftwareUpgrade_new() {
         wd.get("http://10.10.9.13/login.html");
         wd.findElement(By.id("txtLogin")).click();
         wd.findElement(By.id("txtLogin")).clear();
@@ -27,6 +28,7 @@ public class SoftwareUpgrade {
         wd.findElement(By.id("txtPassword")).clear();
         wd.findElement(By.id("txtPassword")).sendKeys("1234");
         wd.findElement(By.id("submitBtn")).click();
+        wd.findElement(By.xpath("//div[@id='vms-export-container']//button[.='Close']")).click();
         wd.findElement(By.linkText("Configuration")).click();
         wd.findElement(By.cssSelector("span.vicon-font.v-software-upgrade")).click();
         wd.findElement(By.xpath("//div[@class='vms-right-panel-content']//button[.='click here']")).click();
